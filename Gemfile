cd17bf154bcfaaff00c38b8405349dbe3e0fcd4c
fthylmz <nuevo.raki@gmail.com>
2013-03-24 22:10:15 +0200

    ilk katkı

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'spree', :git => 'git@github.net:spree/spree.git', :branch => '1-3-stable'
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+end
+group :development do
+	gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZibIn::Application.config.secret_token = '1b3c614dd9d8e47f85c89ef2b95c3585dea50c374e71fe37461ea29777343b2ec71e88fa69f70a645444604583ce59ef65d4ed73b9d18afd62dce15471400492'
